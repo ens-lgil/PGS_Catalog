506bf493bc2e6f37aef2fdeb9bf52e05cd8e3eb8
Laurent Gil <lg10@sanger.ac.uk>
2020-11-03 09:08:30 +0000

    Update display and fix minor bugs for the Upload of the metadata file

--- catalog/static/catalog/pgs_metadata_check.js
+++ catalog/static/catalog/pgs_metadata_check.js
@@ -1,13 +1,13 @@
-var check_url = 'http://127.0.0.1:5000/validate';
-//var storage_url = 'https://validator-dot-pgs-catalog.appspot.com/validate';
+//var validation_service_url = 'http://127.0.0.1:5000/validate';
+var validation_service_url = 'https://validator-dot-pgs-catalog.appspot.com/validate';
 
 $(document).ready(function() {
   var filename = $('#uploaded_filename').attr('data-value');
   console.log('filename: '+filename);
   if (filename) {
-    console.log('check_url: '+check_url);
+    console.log('validation_service_url: '+validation_service_url);
     $.ajax({
-        url: check_url,
+        url: validation_service_url,
         method: "POST",
         contentType: "application/json",
         dataType: 'json',
@@ -16,7 +16,6 @@ $(document).ready(function() {
     .done(function (data) {
       console.log("SUCCESS");
       var status_style = (data.status == 'failed') ? '<i class="fa fa-times-circle-o" style="color:#A00;font-size:16px"></i> Failed' : '<i class="fa fa-check-circle-o" style="color:#0A0"></i> Passed';
-      //var status_html = '<div class="mb-3"><b>File check:</b> '+status_style+'</div>';
       var status_html = '<table class="table table-bordered table_pgs_h mb-4"><tbody>'+
                         '  <tr><td>File check</td><td>'+status_style+'</td></tr>'+
                         '</tbody></table>';
@@ -35,6 +34,9 @@ $(document).ready(function() {
         report += '</tbody></table>';
         $('#report_error').html(report);
       }
+      else {
+        $('#report_error').html('');
+      }
       // Warning messages
       if (data.warning) {
         var report = '<h5 class="mt-4" style="color:orange">Warning report</h5>'+
@@ -49,20 +51,30 @@ $(document).ready(function() {
         report += '</tbody></table>';
         $('#report_warning').html(report);
       }
+      else {
+        $('#report_warning').html('');
+      }
     })
     .fail(function (xhRequest, ErrorText, thrownError) {
       var status_html = '<div><b>File check:</b> <i class="fa fa-times-circle-o" style="color:#A00"></i> Failed</div>';
       $('#check_status').html(status_html);
-      $('#check_report').html(thrownError);
+      error_msg = (thrownError && thrownError != '') ? thrownError : 'Internal error';
+      var error_html = '<div class="clearfix">'+
+                        '  <div class="mt-3 float_left pgs_note pgs_note_2">'+
+                        '    <div><b>Error:</b> '+error_msg+'</div>'+
+                        '  </div>'+
+                        '</div>';
+      $('#report_error').html(error_html);
     });
   }
   else {
-    var status_html = '<div class="clearfix">'+
+    var error_html = '<div class="clearfix">'+
                       '  <div class="mt-3 float_left pgs_note pgs_note_2">'+
-                      '    <div><span>Error:</span> The upload of the file "'+filename+'" failed.</div>'+
+                      '    <div><b>Error:</b> The upload of the file "'+filename+'" failed.</div>'+
                       '  </div>'+
                       '</div>';
-    $('#check_status').html(status_html);
+    $('#check_status').html('');
+    $('#report_error').html(error_html);
   }
 
 
