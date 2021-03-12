b2788d91e8e4e116b2a515a45095b49d631b440c
Laurent Gil <lg10@sanger.ac.uk>
2021-03-12 16:33:41 +0000

    Update models and tests

--- benchmark/models.py
+++ benchmark/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.postgres.fields import DecimalRangeField
 
